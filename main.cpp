8475879bbdbdbd79d26dd35ce7c843d9e09cb02b
KWilson718 <wilsonk18@spu.edu>
2024-02-28 13:26:03 -0800

    Core Extract Functionality Works, Writes to Console

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using std::cin;
 using std::endl;
 using std::string;
 using std::size_t;
+using std::ifstream;
 
 
 string ExtractData(string fileName, size_t start, size_t end);
@@ -17,6 +18,8 @@ int main(int argc, char* argv[]) {
     int start;
     int end;
 
+    string extractedData;
+
     
     while (StillGoing){
         cout << "Enter the File Name & Path that you'd like to convert" << endl;
@@ -27,6 +30,11 @@ int main(int argc, char* argv[]) {
         cin >> end;
 
         cout << "Searching for a file called: \"" << inputFileName << "\" and will start at positon " << start << " then capturing the text through position " << end << "." << endl;
+
+        extractedData = ExtractData(inputFileName, start, end);
+
+        cout << "Found the Data: " << extractedData << endl;
+
         StillGoing = false;
     }
 
@@ -34,5 +42,26 @@ int main(int argc, char* argv[]) {
 }
 
 string ExtractData(string fileName, size_t start, size_t end){
+    string dataString;
+    size_t dataLength = (end - start);
+    ifstream fileInput;
+    fileInput.open(fileName);
 
+    if (fileInput.is_open()){
+        if (start > 1){
+            fileInput.ignore(start - 1, EOF);
+        }
+
+        for (size_t i = 0; i <= dataLength; i++){
+            char c = fileInput.get();
+            if (c != EOF){
+                dataString.push_back(c);
+            }
+            else{
+                break;
+            }
+        }
+    }
+    fileInput.close();
+    return dataString;
 }
